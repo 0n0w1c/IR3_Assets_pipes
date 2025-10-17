9e5f7dd92470348a5a1792625bbbf4bf6bc520d7
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2025-10-17 09:02:37 -0500

    Version 0.0.1

--- prototypes/entity/pipe-to-ground.lua
+++ prototypes/entity/pipe-to-ground.lua
@@ -1,8 +1,32 @@
-local pipecovers = require("prototypes/entity/pipecovers")
-local pictures = require("prototypes/entity/pipe-to-ground-pictures")
+local pipe_covers = require("prototypes/entity/pipe-covers")
 
 local entity = data.raw["pipe-to-ground"]["pipe-to-ground"]
 
 entity.icon = "__IndustrialRevolution3Assets1__/graphics/icons/64/pipe-to-ground.png"
-entity.fluid_box.pipe_covers = pipecovers
-entity.pictures = pictures
+entity.fluid_box.pipe_covers = pipe_covers
+entity.pictures = {
+    north = {
+        layers = {
+            { filename = "__IndustrialRevolution3Assets2__/graphics/entities/pipes/pipe-iron-un.png",   width = 180, height = 120, priority = "extra-high", scale = 0.5 },
+            { filename = "__IndustrialRevolution3Assets2__/graphics/entities/pipes/pipe-shadow-un.png", width = 180, height = 120, priority = "extra-high", scale = 0.5, draw_as_shadow = true }
+        }
+    },
+    east = {
+        layers = {
+            { filename = "__IndustrialRevolution3Assets2__/graphics/entities/pipes/pipe-iron-ue.png",   width = 180, height = 120, priority = "extra-high", scale = 0.5 },
+            { filename = "__IndustrialRevolution3Assets2__/graphics/entities/pipes/pipe-shadow-ue.png", width = 180, height = 120, priority = "extra-high", scale = 0.5, draw_as_shadow = true }
+        }
+    },
+    south = {
+        layers = {
+            { filename = "__IndustrialRevolution3Assets2__/graphics/entities/pipes/pipe-iron-us.png",   width = 180, height = 120, priority = "extra-high", scale = 0.5 },
+            { filename = "__IndustrialRevolution3Assets2__/graphics/entities/pipes/pipe-shadow-us.png", width = 180, height = 120, priority = "extra-high", scale = 0.5, draw_as_shadow = true }
+        }
+    },
+    west = {
+        layers = {
+            { filename = "__IndustrialRevolution3Assets2__/graphics/entities/pipes/pipe-iron-uw.png",   width = 180, height = 120, priority = "extra-high", scale = 0.5 },
+            { filename = "__IndustrialRevolution3Assets2__/graphics/entities/pipes/pipe-shadow-uw.png", width = 180, height = 120, priority = "extra-high", scale = 0.5, draw_as_shadow = true }
+        }
+    }
+}
