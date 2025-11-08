45c8b89bd5c75960a6eb156b4552313ca5081aa5
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2025-11-08 15:31:41 -0600

    Version 0.1.1

--- data-updates.lua
+++ data-updates.lua
@@ -21,3 +21,19 @@ if settings.startup["IR3-enable-small-storage-tank"].value == true then
     require("prototypes/recipe/small-storage-tank")
     require("prototypes/technology/fluid-handling")
 end
+
+if mods["quality"] then
+    local recycling = require("__quality__/prototypes/recycling")
+    local recipe
+
+    recipe = data.raw["recipe"]["pipe"]
+    if recipe then recycling.generate_recycling_recipe(recipe) end
+
+    recipe = data.raw["recipe"]["pipe-to-ground"]
+    if recipe then recycling.generate_recycling_recipe(recipe) end
+
+    if settings.startup["IR3-enable-small-storage-tank"].value == true then
+        recipe = data.raw["recipe"]["small-storage-tank"]
+        if recipe then recycling.generate_recycling_recipe(recipe) end
+    end
+end
